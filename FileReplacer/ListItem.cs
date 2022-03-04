7054b04aa33c787c06d7f114f44f441e59c8f0df
Owes <mohammed.owes@archerirm.com>
2022-03-04 21:41:24 +0530

    Add project files.

---
+++ FileReplacer/ListItem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+
+namespace FileReplacer
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            DestinationValues = new ObservableCollection<Destination>();
+            SourceValues = new ObservableCollection<Source>();
+        }
+
+        public ObservableCollection<Destination> DestinationValues
+        {
+            get;
+            set;
+        }
+        public ObservableCollection<Source> SourceValues
+        {
+            get;
+            set;
+        }
+    }
+    public class Destination
+    {
+        public string Path
+        {
+            get;
+            set;
+        }
+    }
+    public class Source
+    {
+        public string Path
+        {
+            get;
+            set;
+        }
+    }
+}
